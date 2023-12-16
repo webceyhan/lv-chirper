6a17e9ffcd73fde18cf41ebd98ebad4182654580
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-12-16 18:40:06 +0100

    add chirp-created event for mail notification

---
+++ app/Listeners/SendChirpCreatedNotifications.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ChirpCreated;
+use App\Models\User;
+use App\Notifications\NewChirp;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendChirpCreatedNotifications implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(ChirpCreated $event): void
+    {
+        // send notifications to every user in the platform, except the author of the Chirp
+        foreach (User::whereNot('id', $event->chirp->user_id)->cursor() as $user) {
+            $user->notify(new NewChirp($event->chirp));
+        }
+    }
+}
