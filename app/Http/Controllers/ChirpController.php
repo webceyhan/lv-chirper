65b5cd2602ff8ebf5cbd0cea50caf26cea057a45
Evren Ceyhan <evren.ceyhan@gmail.com>
2023-12-16 18:12:03 +0100

    add chirps/destroy endpoint

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -79,6 +79,10 @@ class ChirpController extends Controller
      */
     public function destroy(Chirp $chirp)
     {
-        //
+        $this->authorize('delete', $chirp);
+
+        $chirp->delete();
+
+        return redirect(route('chirps.index'));
     }
 }
--- app/Policies/ChirpPolicy.php
+++ app/Policies/ChirpPolicy.php
@@ -46,7 +46,7 @@ class ChirpPolicy
      */
     public function delete(User $user, Chirp $chirp): bool
     {
-        return true;
+        return $this->update($user, $chirp);
     }
 
     /**
